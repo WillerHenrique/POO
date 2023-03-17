89ec401d79420b1f40e02d5e62079dadd216fc40
12114814 <willerhenrique100@gmail.com>
2023-03-17 20:13:10 -0300

    feat: conta atualizada

--- src/br/newtonpaiva/dominio/Conta.java
+++ src/br/newtonpaiva/dominio/Conta.java
@@ -1,19 +1,62 @@
 package br.newtonpaiva.dominio;
 
-public class Conta implements Transferencia {
+import java.util.Objects;
+
+public class Conta {
     private Integer numero;
-    protected Double saldo;
+    private Double saldo;
+    public Conta() {
+        this(null, null);
+    }
+    public Conta(Integer numero) {
+        this.numero = numero;
+        this.saldo = 0.0;
+    }
+
+    public Conta(Integer numero, Double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
 
     public Double sacar(Double valor) {
         return 0.0;
     }
+
     public Double depositar(Double valor) {
-        return 0.0;
+        if (valor == null || valor <= 0)
+            throw new IllegalArgumentException("Por favor digite um valor inteiro, sendo maior de 0");
+        else {
+            saldo = saldo + valor;
+            return saldo;
+        }
     }
+
     public Integer getNumero() {
         return numero;
     }
+
     public void setNumero(Integer numero) {
         this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return Objects.equals(numero, conta.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
     }
 }
