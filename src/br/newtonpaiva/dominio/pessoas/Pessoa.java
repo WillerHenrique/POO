47d7fcfd3b14a09afc2ea89e18b5fca5544652d5
12114814 <willerhenrique100@gmail.com>
2023-03-17 20:14:22 -0300

    feat: conta atualizada

---
+++ src/br/newtonpaiva/dominio/pessoas/Pessoa.java
@@ -0,0 +1,16 @@
+package br.newtonpaiva.dominio.pessoas;
+
+public class Pessoa {
+    private String nome;
+
+    public void validarDocumento() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
